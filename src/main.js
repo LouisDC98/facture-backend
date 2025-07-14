aff7a6b10308a910de115b3e52735aee77a9f2d7
LouisDC <louisdacosta98@gmail.com>
2025-07-14 13:26:32 +0200

    Add CORS

--- src/main.js
+++ src/main.js
@@ -1,33 +1,35 @@
-import  express from 'express'
+import express from 'express'
 import usersRoutes from './routes/users.route.js';
 import Database from './db.js'
+import cors from 'cors'
 
 
 async function startApp() {
-  const app = express();
-  app.use(express.json({ limit: '50mb' }));
-  app.use(
-    express.urlencoded({
-      extended: false,
-      limit: '50mb',
-    })
-  );
+    const app = express();
+    app.use(cors())
+    app.use(express.json({ limit: '50mb' }));
+    app.use(
+        express.urlencoded({
+            extended: false,
+            limit: '50mb',
+        })
+    );
 
-  app.use('/users', usersRoutes);
+    app.use('/users', usersRoutes);
 
-  console.log('FACTURE SERVER STARTING...');
+    console.log('FACTURE SERVER STARTING...');
 
-  // INIT DATABASE CONNECTION
-  try {
-    await Database.connect();
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
+    // INIT DATABASE CONNECTION
+    try {
+        await Database.connect();
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
 
-  app.listen(process.env.APP_PORT || 3000, () => {
-    console.log('SERVER RUNNING ON PORT ' + (process.env.APP_PORT || 3000));
-  });
+    app.listen(process.env.APP_PORT || 3000, () => {
+        console.log('SERVER RUNNING ON PORT ' + (process.env.APP_PORT || 3000));
+    });
 }
 
 startApp();
