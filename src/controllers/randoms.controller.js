493618cd5f3b4fb57ecb1b57dc72c827743d63ef
LouisDC <louisdacosta98@gmail.com>
2025-07-15 12:39:00 +0200

    Update and delete random

--- src/controllers/randoms.controller.js
+++ src/controllers/randoms.controller.js
@@ -14,9 +14,9 @@ class RandomsController {
         }
     }
 
-        static async insertRandom(req, res) {
+    static async insertRandom(req, res) {
         const body = req.body;
-        
+
         try {
             const [result] = await Database.connection.query(
                 `INSERT INTO randoms (
@@ -40,6 +40,56 @@ class RandomsController {
             res.status(201).json({ message: 'Insertion réussie', insertId: result.insertId });
         } catch (error) {
             console.error('Erreur SQL :', error);
+            res.status(500).json({ type: 'Erreur', message: error.message });
+        }
+    }
+
+    static async removeRandom(req, res) {
+        const code = req.params.id;
+
+        try {
+            const [result] = await Database.connection.query(
+                'DELETE FROM `randoms` WHERE `code` = ?',
+                [code]
+            );
+
+            if (result.affectedRows === 0) {
+                return res.status(404).json({ type: 'NotFound', message: 'Aucun article trouvé avec ce code.' });
+            }
+
+            res.status(200).json({ message: 'Article supprimé avec succès.' });
+        } catch (error) {
+            res.status(500).json({ type: 'Erreur', message: error.message });
+        }
+    }
+
+    static async updateRandom(req, res) {
+        const code = req.params.id;
+        const updatedData = req.body;
+
+        try {
+            const [result] = await Database.connection.query(
+                `
+      UPDATE randoms 
+      SET libelle = ?, qtyCmd = ?, tva = ?, prixUnit = ?, prixRemise = ?
+      WHERE code = ?
+      `,
+                [
+                    updatedData.libelle,
+                    updatedData.qtyCmd,
+                    updatedData.tva,
+                    updatedData.prixUnit,
+                    updatedData.prixRemise,
+                    code
+                ]
+            );
+
+            if (result.affectedRows === 0) {
+                return res.status(404).json({ type: 'NotFound', message: 'Aucun article trouvé avec ce code.' });
+            }
+
+            res.status(200).json({ message: 'Article modifié avec succès.' });
+        } catch (error) {
             res.status(500).json({ type: 'Erreur', message: error.message });
         }
     }
--- src/routes/randoms.route.js
+++ src/routes/randoms.route.js
@@ -5,5 +5,7 @@ const randomsRoutes = Router();
 
 randomsRoutes.get('/', Randomscontroller.getAll);
 randomsRoutes.post('/', Randomscontroller.insertRandom);
+randomsRoutes.delete('/:id', Randomscontroller.removeRandom);
+randomsRoutes.put('/:id', Randomscontroller.updateRandom);
 
 export default randomsRoutes;
